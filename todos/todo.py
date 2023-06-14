668b5b2bc197f7f716c2b3c7eb28268ca1930ac4
joonpyo <jjpyo1009@gmail.com>
2023-06-14 23:55:35 +0900

    feat: put 및 delete 기능 추가

--- todos/todo.py
+++ todos/todo.py
@@ -40,3 +40,16 @@ async def update_todo(todo_data: TodoItem, todo_id: int = Path(...,title="The ID
     return {
         "message" : "Todo with supplied ID doesn't exist."
     }
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {
+                "message": "Todo deleted successfully."
+            }
+    return {
+        "message": "Todo with suppled ID doesn't sexist."
+    }
