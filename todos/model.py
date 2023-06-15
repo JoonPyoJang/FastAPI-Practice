4a4ea13778f1a4af078a22a1df0bb25e5e2a7cf2
joonpyo <jjpyo1009@gmail.com>
2023-06-15 19:35:23 +0900

    feat: 응답모델 및 오류 처리

--- todos/model.py
+++ todos/model.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List
 
 class Todo(BaseModel):
     id: int
@@ -18,8 +19,23 @@ class TodoItem(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "example": {
-                    "item": "Read the next chapter of the book."
-                }
+                "item": "Read the next chapter of the book."
+            }
+        }
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example" : {
+                "todo": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
             }
         }
